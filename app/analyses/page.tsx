0bad8ad9e929dfc128418b8f2ba7ae635a7e8b4b
Aptiq <joey.montani@aptiq.ch>
2025-01-23 16:59:31 +0100

    24

--- app/analyses/page.tsx
+++ app/analyses/page.tsx
@@ -1,7 +1,9 @@
 import { prisma } from '@/lib/prisma'
 import { PageHeader } from '@/components/layout/page-header'
-import { AnalysesList } from '@/components/analyses-list'
-import { CreateAnalysisButton } from '@/components/create-analysis-button'
+import { AnalysisList } from '@/components/analysis/analysis-list'
+import { Button } from '@/components/ui/button'
+import Link from 'next/link'
+import { PlusCircle } from 'lucide-react'
 import type { Analysis } from '@prisma/client'
 
 export default async function AnalysesPage() {
@@ -27,10 +29,15 @@ export default async function AnalysesPage() {
         title="Analyses"
         description="Liste de toutes les analyses"
       >
-        <CreateAnalysisButton />
+        <Button asChild>
+          <Link href="/analyses/new">
+            <PlusCircle className="mr-2 h-4 w-4" />
+            Nouvelle analyse
+          </Link>
+        </Button>
       </PageHeader>
 
-      <AnalysesList analyses={analyses} />
+      <AnalysisList analyses={analyses} />
     </div>
   )
 } 
